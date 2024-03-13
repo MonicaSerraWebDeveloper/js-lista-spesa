fe8dad6718dfc77fcf592192018f15b16e017612
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-13 14:02:59 +0100

    start init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
